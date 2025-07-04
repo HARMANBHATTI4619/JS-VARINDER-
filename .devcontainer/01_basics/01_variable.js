bfdd51d51595f64adb55cdd9ef65d2d60080b76a
HARMANBHATTI4619 <preets4619@gmail.com>
2025-07-04 06:12:43 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,22 @@
+const accountID =144553
+let accountEmail="preets4619@gmail.com"
+var accountPassword= "123456"
+accountCity = "Ropar"
+let accountState;
+
+
+// accountID = 2 // Not allowed
+
+accountEmail= "hc@hc.com"
+accountPassword= "258963145"
+accountCity= "Bengaluru"
+
+console.log(accountID);
+
+/*
+Perfer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState]) 
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strick"; // treat all JS code as newer version
+
+// alert (3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability be high
+
+console.log ("harman gg")
+
+
+let name = "Navdeep"
+let age = 25
+let isloggedIN = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standloan value
+// undefined =>  
+// symbol => unique
+
+
+// object
+
+console.log (typeof null); // object
+console.log (typeof undefined); // undefined

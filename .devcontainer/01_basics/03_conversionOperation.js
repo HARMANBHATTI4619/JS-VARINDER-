4ba82a0e40bc859b0b6c5cafb5958b592a0792c2
HARMANBHATTI4619 <preets4619@gmail.com>
2025-07-06 06:39:25 +0000

    conversion of data

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueINNumber = Number(score)
+// console.log(typeof valueINNumber );
+// console.log(valueINNumber);
+
+// "33" => 33
+// "33abc"  => NaN
+// true => 1; false =>0
+
+let isloggedIn = "harman"
+
+let booleanIsloggedIn = Boolean(isloggedIn)
+// console.log(booleanIsloggedIn);
+
+// 1 => true; 0 => fasle
+// "" => false
+// "harman" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);

f8f9ef6af426296216a40533827ba6245ab1f2da
HARMANBHATTI4619 <preets4619@gmail.com>
2025-07-07 10:51:40 +0000

    conversionOperation

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -25,3 +25,33 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+
+// * **************************************************Operation***************************
+
+let value = 3
+let negValue = -value
+// console.log (negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " harman"
+
+let str3 = str1 + str2
+console.log(str3);
+
+let num1, num2, num3
+
+num1= num2= num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
